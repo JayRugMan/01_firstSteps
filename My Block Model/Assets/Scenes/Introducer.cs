3cfe338527dae2a43924d7bf447ba339f39b8dff
jayrugman <jasonrhardman@gmail.com>
2020-05-29 00:06:16 -0600

    Just finishing section 1 of the Udemy course.

--- My Block Model/Assets/Scenes/Introducer.cs
+++ My Block Model/Assets/Scenes/Introducer.cs
@@ -7,7 +7,8 @@ public class Introducer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello Jason. Pick a car.");
+        print("... then go for a spin");
     }
 
     // Update is called once per frame
